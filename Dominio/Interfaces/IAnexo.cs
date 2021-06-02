b71a0b8250a5d16c525c2f69a2ce73165e343524
Igor Marcante <08488726902@rondonia.local>
2021-06-02 13:22:32 -0400

    first commit

---
+++ Dominio/Interfaces/IAnexo.cs
@@ -0,0 +1,9 @@
+namespace Dominio.Interfaces
+{
+    public interface IAnexo
+    {   
+        string Mime { get; }      
+        string Nome { get; }      
+        string LocalizadorDoAnexo { get; }                  
+    }
+}
